b6a5cabbb05df952c8e0df4d6c6bd0588796807d
Denys Vuika <denis.vuyka@gmail.com>
2017-08-10 16:57:40 +0100

    document list configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { CoreModule } from 'ng2-alfresco-core';
 import { DataTableModule } from 'ng2-alfresco-datatable';
 import { ViewerModule } from 'ng2-alfresco-viewer';
+import { DocumentListModule } from 'ng2-alfresco-documentlist';
 
 // App components
 import { AppComponent } from './app.component';
 import { DatatableComponent } from './datatable/datatable.component';
 import { HomeComponent } from './home/home.component';
 import { ViewerComponent } from './viewer/viewer.component';
+import { DocumentlistComponent } from './documentlist/documentlist.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'datatable', component: DatatableComponent },
-  { path: 'viewer', component: ViewerComponent }
+  { path: 'viewer', component: ViewerComponent },
+  { path: 'documentlist', component: DocumentlistComponent }
 ];
 
 @NgModule({
@@ -24,19 +27,21 @@ const appRoutes: Routes = [
     AppComponent,
     DatatableComponent,
     HomeComponent,
-    ViewerComponent
+    ViewerComponent,
+    DocumentlistComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
+      appRoutes // ,
+      // { enableTracing: true } // <-- debugging purposes only
     ),
 
     // ADF component modules
     CoreModule,
     DataTableModule,
-    ViewerModule
+    ViewerModule,
+    DocumentListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
