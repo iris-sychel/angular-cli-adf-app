e762b41210da20089ea904f412cdb6ad1cac431f
Denys Vuika <denis.vuyka@gmail.com>
2017-09-11 15:14:00 +0100

    npm scripts and i18n enhancements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlfrescoTranslationService } from 'ng2-alfresco-core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(translationService: AlfrescoTranslationService) {
+    translationService.use('en');
+  }
 
 }
